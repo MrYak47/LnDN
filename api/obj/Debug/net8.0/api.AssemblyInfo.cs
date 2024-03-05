70b28bb1808722862c697b95be17e334abb48a1e
MrYak47 <jacobs.bongo@gmail.com>
2024-03-05 18:38:19 +0200

    sqlsetup_18370305

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d95db54d74c218c18e9169112ed337653c79825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66354c37aee5afab353757e1cbb3725b0a968e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
